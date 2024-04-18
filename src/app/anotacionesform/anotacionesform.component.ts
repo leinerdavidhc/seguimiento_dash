d42d02d4e21bb80a0f92c17065a45caf680b0ff7
leinerdavidhc <leinerdavidhc20@gmail.com>
2024-04-18 12:46:42 -0500

    add form getform

--- src/app/anotacionesform/anotacionesform.component.ts
+++ src/app/anotacionesform/anotacionesform.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 interface TipoI {
   tipo: string;
 }
@@ -7,15 +8,28 @@ interface TipoI {
 @Component({
   selector: 'app-anotacionesform',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './anotacionesform.component.html',
   styleUrl: './anotacionesform.component.css'
 })
 export class AnotacionesformComponent implements OnInit{
   Tipos: TipoI[] | undefined;
-  dateTime: string = '';
+  dateTime1: string = '';
+  dateTime2: string = '';
+  form:FormGroup;
   
+  constructor(private fb:FormBuilder){
+    this.form=this.fb.group({
+      grupo_tipo:['', Validators.required],
+      grupo_cantidad:[1,Validators.required],
+      date_inicio:['',Validators.required],
+      time_inicio:['',Validators.required],
+      date_fin:['',Validators.required],
+      time_fin:['',Validators.required],
+      observacion:['',Validators.required]
 
+    })
+  }
   ngOnInit(): void {
     this.Tipos = [
       { tipo: 'automovil' },
@@ -24,20 +38,36 @@ export class AnotacionesformComponent implements OnInit{
       { tipo: 'moto' },
       { tipo: 'otros' },
     ];
-    this.addDateTime();
+    this.addDateTime(1);
+    this.addDateTime(2)
+    this.form.get("time_inicio")?.setValue(this.dateTime1)
+    this.form.get("time_fin")?.setValue(this.dateTime2)
   }
 
-  addDateTime() {
+  addDateTime(value: number) {
     const now = new Date(); // Obtener la fecha y hora actuales
     const hours = this.formatNumber(now.getHours()); // Obtener las horas
     const minutes = this.formatNumber(now.getMinutes()); // Obtener los minutos
     const seconds = this.formatNumber(now.getSeconds()); // Obtener los segundos
 
     // Formatear la fecha y hora en el formato deseado (HH:MM:SS)
-    this.dateTime = `${hours}:${minutes}:${seconds}`;
+
+    if (value==1) {
+      this.dateTime1 = `${hours}:${minutes}:${seconds}`;
+    }else{
+      this.dateTime2 = `${hours}:${minutes}:${seconds}`;
+    }
+    
   }
 
   formatNumber(num: number): string {
     return num < 10 ? '0' + num : num.toString();
+  }
+
+  getDataForm(){
+    console.log(this.form.value);
+    this.form.reset();
+    this.form.get("time_inicio")?.setValue(this.dateTime1)
+    this.form.get("time_fin")?.setValue(this.dateTime2)
   }
 }
--- src/app/seguimientoform/seguimientoform.component.ts
+++ src/app/seguimientoform/seguimientoform.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 interface TipoI {
   tipo: string;
 }
@@ -9,14 +10,29 @@ interface DireccionI {
 @Component({
   selector: 'app-seguimientoform',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './seguimientoform.component.html',
   styleUrl: './seguimientoform.component.css'
 })
 export class SeguimientoformComponent implements OnInit{
   Tipos: TipoI[] | undefined;
   Direccion: DireccionI[] | undefined;
-  dateTime: string = '';
+  dateTime1: string = '';
+  dateTime2: string = '';
+  form:FormGroup;
+
+  constructor(private fb:FormBuilder){
+    this.form=this.fb.group({
+      tipo:['', Validators.required],
+      direccion:['',Validators.required],
+      cantidad:[1,Validators.required],
+      date_inicio:['',Validators.required],
+      time_inicio:['',Validators.required],
+      date_fin:['',Validators.required],
+      time_fin:['',Validators.required]
+
+    })
+  }
   
 
   ngOnInit(): void {
@@ -38,20 +54,37 @@ export class SeguimientoformComponent implements OnInit{
       { direccion: 'derecha-arriba ⬑' },
       { direccion: 'abajo ⭣' },
     ];
-    this.addDateTime();
+    this.addDateTime(1);
+    this.addDateTime(2);
+
+    this.form.get("time_inicio")?.setValue(this.dateTime1)
+    this.form.get("time_fin")?.setValue(this.dateTime2)
   }
 
-  addDateTime() {
+  addDateTime(value: number) {
     const now = new Date(); // Obtener la fecha y hora actuales
     const hours = this.formatNumber(now.getHours()); // Obtener las horas
     const minutes = this.formatNumber(now.getMinutes()); // Obtener los minutos
     const seconds = this.formatNumber(now.getSeconds()); // Obtener los segundos
 
     // Formatear la fecha y hora en el formato deseado (HH:MM:SS)
-    this.dateTime = `${hours}:${minutes}:${seconds}`;
+
+    if (value==1) {
+      this.dateTime1 = `${hours}:${minutes}:${seconds}`;
+    }else{
+      this.dateTime2 = `${hours}:${minutes}:${seconds}`;
+    }
+    
   }
 
   formatNumber(num: number): string {
     return num < 10 ? '0' + num : num.toString();
+  }
+
+  getDataForm(){
+    console.log(this.form.value);
+    this.form.reset();
+    this.form.get("time_inicio")?.setValue(this.dateTime1)
+    this.form.get("time_fin")?.setValue(this.dateTime2)
   }
 }
